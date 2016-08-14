9ed0ff0e79888e48e373726cd43a3c64f9456b90
unknown <minh.lq7@gmail.com>
2016-08-15 00:02:56 +0700

    abc

--- auto pull push script.py
+++ auto pull push script.py
@@ -9,20 +9,27 @@ if fileOrFiles == 'one':
 elif fileOrFiles == 'all':
     myFiles = ['FE','git','Japanese','java','Lap trinh shell','linux','lpic1','lpic2','network','python','raspberry-pi','selenium','software','testing','training']
 
-condition = input('pull or add or commit or push: ')
-if condition == 'pull':
-    for fileName in myFiles:
-        currentDir = os.chdir(os.path.join(defaultPath,fileName))
-        subprocess.Popen(['cmd','/K','git pull origin master'])
-elif condition == 'add':
-    for fileName in myFiles:
-        currentDir = os.chdir(os.path.join(defaultPath,fileName))
-        subprocess.Popen(['cmd','/K','git add *'])     
-elif condition == 'commit':
-    for fileName in myFiles:
-        currentDir = os.chdir(os.path.join(defaultPath,fileName))
-        subprocess.Popen(['cmd','/K','git commit -m abc'])
-elif condition == 'push':
-    for fileName in myFiles:
-        currentDir = os.chdir(os.path.join(defaultPath,fileName))
-        subprocess.Popen(['cmd','/K','git push origin master'])
+while True:
+    condition = input('pull or add or commit or push or exit: ')
+    if condition == 'pull':
+        for fileName in myFiles:
+            currentDir = os.chdir(os.path.join(defaultPath,fileName))
+            subprocess.Popen(['cmd','/K','git pull origin master'])
+        continue
+    elif condition == 'add':
+        for fileName in myFiles:
+            currentDir = os.chdir(os.path.join(defaultPath,fileName))
+            subprocess.Popen(['cmd','/K','git add *'])
+        continue
+    elif condition == 'commit':
+        for fileName in myFiles:
+            currentDir = os.chdir(os.path.join(defaultPath,fileName))
+            subprocess.Popen(['cmd','/K','git commit -m abc'])
+        continue
+    elif condition == 'push':
+        for fileName in myFiles:
+            currentDir = os.chdir(os.path.join(defaultPath,fileName))
+            subprocess.Popen(['cmd','/K','git push origin master'])
+        continue
+    elif condition == 'exit':
+        break
