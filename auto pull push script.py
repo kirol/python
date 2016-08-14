cd331ce842d9c4e5b325ce07bf86ab2fb6c1f321
unknown <minh.lq7@gmail.com>
2016-08-15 00:00:50 +0700

    abc

--- auto pull push script.py
+++ auto pull push script.py
@@ -9,18 +9,20 @@ if fileOrFiles == 'one':
 elif fileOrFiles == 'all':
     myFiles = ['FE','git','Japanese','java','Lap trinh shell','linux','lpic1','lpic2','network','python','raspberry-pi','selenium','software','testing','training']
 
-condition = input('pull or push: ')
+condition = input('pull or add or commit or push: ')
 if condition == 'pull':
     for fileName in myFiles:
-        os.chdir(os.path.join(defaultPath,fileName))
+        currentDir = os.chdir(os.path.join(defaultPath,fileName))
         subprocess.Popen(['cmd','/K','git pull origin master'])
+elif condition == 'add':
+    for fileName in myFiles:
+        currentDir = os.chdir(os.path.join(defaultPath,fileName))
+        subprocess.Popen(['cmd','/K','git add *'])     
+elif condition == 'commit':
+    for fileName in myFiles:
+        currentDir = os.chdir(os.path.join(defaultPath,fileName))
+        subprocess.Popen(['cmd','/K','git commit -m abc'])
 elif condition == 'push':
     for fileName in myFiles:
-        os.chdir(os.path.join(defaultPath,fileName))
-        gitAdd = subprocess.Popen(['cmd','/K','git add *'])
-        gitCommit = subprocess.Popen(['cmd','/K','git commit -m abc'])
-        gitPush = subprocess.Popen(['cmd','/K','git push origin master'])
- 
-        
-
-    
+        currentDir = os.chdir(os.path.join(defaultPath,fileName))
+        subprocess.Popen(['cmd','/K','git push origin master'])
